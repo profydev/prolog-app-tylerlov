01e86928312d5e3430cb23df08281e0b6e8d328a
Tyler Lovell <tylerlovell@gmail.com>
2023-11-27 21:59:41 -0500

    Major CSS additions, need to refine to finish

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,86 +8,106 @@ const IssuesPage = () => {
     <div>
       <header className={styles.header}>
         <Image
+          className={styles.logo}
           src="/icons/logo-large.svg"
           alt="Prolog logo"
-          width={500}
-          height={300}
+          width={118}
+          height={33}
         />
         <Header />
         <a href={Routes.projects}>Dashboard</a>
       </header>
-      <h1>Your Issues In Sight. At All Times.</h1>
-      <p>
-        Powerful error tracking and monitoring for software applications.
-        Trusted by over 4,000 startups.
-      </p>
-      <Image
-        src="/path/to/your/image.jpg"
-        alt="Screen Mockup"
-        width={500}
-        height={300}
-      />
+      <div className={styles.headingContainer}>
+        <h1 className={styles.headingText}>
+          Your Issues In Sight. At All Times.
+        </h1>
+        <div className={styles.supportingTextWrap}>
+          <p className={styles.supportingText}>
+            Powerful error tracking and monitoring for software applications.
+            Trusted by over 4,000 startups.
+          </p>
+        </div>
+        <Image
+          className={styles.computer}
+          src="/images/Macbook Pro 16 mockup.png"
+          alt="Screen Mockup"
+          width={753.676}
+          height={445.894}
+        />
+      </div>
 
       {/* Social Proof Section */}
       <section>
-        <h2>Join 4,000+ companies using Prolog</h2>
-        <div className={styles.socialProofContainer}>
+        <h4 className={styles.textSmall}>Join 4,000+ companies using Prolog</h4>
+        <div className={styles.companyContainer}>
           <Image
-            src="/path/to/image1.jpg"
+            className={styles.companyLogo}
+            src="/images/Company logo 1.png"
             alt="Company 1"
-            width={500}
-            height={300}
+            width={146}
+            height={48}
           />
           <Image
-            src="/path/to/image2.jpg"
+            className={styles.companyLogo}
+            src="/images/Company logo 2.png"
             alt="Company 2"
-            width={500}
-            height={300}
+            width={169}
+            height={48}
           />
           <Image
-            src="/path/to/image3.jpg"
+            className={styles.companyLogo}
+            src="/images/Company logo 3.png"
             alt="Company 3"
-            width={500}
-            height={300}
+            width={183}
+            height={48}
           />
           <Image
-            src="/path/to/image4.jpg"
+            className={styles.companyLogo}
+            src="/images/Company logo 4.png"
             alt="Company 4"
-            width={500}
-            height={300}
+            width={160}
+            height={48}
           />
           <Image
-            src="/path/to/image5.jpg"
+            className={styles.companyLogo}
+            src="/images/Company logo 5.png"
             alt="Company 5"
-            width={500}
-            height={300}
+            width={187}
+            height={48}
           />
         </div>
       </section>
 
       {/* Testimonial Section */}
-      <section>
-        <h2>Don&apos;t Only Trust Our Words</h2>
-        <p>Our customers around the globe share their opinions.</p>
+      <div className={styles.testimonialHeading}>
+        <h2 className={styles.headingText}>Don&apos;t Only Trust Our Words</h2>
+        <div className={styles.supportingTextWrap}>
+          <p className={styles.supportingText}>
+            Our customers around the globe share their opinions.
+          </p>
+        </div>
         <div className={styles.testimonialContainer}>
           {[1, 2, 3].map((testimonial, index) => (
             <div key={index} className={styles.testimonial}>
-              <h3>Subhead</h3>
-              <p>&ldquo;Quote&rdquo;</p>
+              <h3>Frontend Development</h3>
+              <p>
+                Prolog has saved us many times. We get an alert, investgate the
+                error, and fix it. That simple.
+              </p>
               <div className={styles.avatarContainer}>
                 <Image
-                  src="/path/to/avatar.jpg"
+                  src="/images/Avatar 1.png"
                   alt="Avatar"
-                  width={500}
-                  height={300}
+                  width={56}
+                  height={56}
                 />
-                <p>Name</p>
-                <p>Profession</p>
+                <p>Mollie Hall</p>
+                <p>Web Developer, Sisyphus</p>
               </div>
             </div>
           ))}
         </div>
-      </section>
+      </div>
 
       <button
         className={styles.contactButton}
@@ -97,12 +117,7 @@ const IssuesPage = () => {
           )
         }
       >
-        <Image
-          src="/icons/message.svg"
-          alt="Contact"
-          width={500}
-          height={300}
-        />
+        <Image src="/icons/message.svg" alt="Contact" width={56} height={56} />
       </button>
     </div>
   );
